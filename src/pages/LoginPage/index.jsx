1ce56c7fd1f339dd34bad15c5ff9abc4aa178a2c
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-10-04 00:34:23 +0900

    💄 LoginPage UI 완료

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import LoginBtn from "../../components/buttons/LoginBtn";
+import LoginArea from "../../components/LoginPage/LoginArea";
 import { LeftSide, LogoImg, RightSide, Wrapper } from "./styles";
 export default function LoginPage() {
   return (
     <Wrapper>
       <LeftSide>
+        <LoginArea />
       </LeftSide>
       <RightSide />
     </Wrapper>
