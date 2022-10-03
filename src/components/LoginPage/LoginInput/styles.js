7b12bab77637a2e738f3ece6121ba0dfd31bf5f3
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-10-04 00:33:53 +0900

    💄 로그인 아이디, 비밀번호 입력창

---
+++ src/components/LoginPage/LoginInput/styles.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  margin-right: 10px;
+  margin-top: 20px;
+`;
