76be911f03e3208e9795a4c6790971bab73c785d
haein.kwon <hae0_02ni@ewhain.net>
2022-10-03 22:33:22 +0900

    ✍️ 💄메인페이지 모달 기능 추가 및 모달 ui 구현

--- src/components/buttons/AuthBtn.jsx
+++ src/components/buttons/AuthBtn.jsx
@@ -1,9 +1,35 @@
+import { useState } from "react";
 import styled from "styled-components";
+import Modal from "../modals/index";
 
 export default function AuthBtn() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  const date = new Date();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
   return (
     <>
-      <Button>인증하기</Button>
+      <Button onClick={openModal}>인증하기</Button>
+      <Modal
+        open={modalOpen}
+        close={closeModal}
+        header={month + "월 " + day + "일 스터디 인증하기"}
+      >
+        <AuthPic>사진을 업로드 해주세요.</AuthPic>
+        <ButtonWrapper>
+          <CloseButton onClick={closeModal}>취소하기</CloseButton>
+          <Button>인증하기</Button>
+        </ButtonWrapper>
+      </Modal>
     </>
   );
 }
@@ -21,4 +47,44 @@ const Button = styled.button`
   font-weight: 700;
   font-size: 14px;
   color: white;
+`;
+
+const CloseButton = styled.button`
+  width: 108px;
+  height: 40px;
+  border-radius: 5px;
+
+  background: #b4b3b3;
+  border: #b4b3b3;
+  text-align: center;
+  font-family: "Pretendard";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  color: white;
+  margin-right: 10px;
+`;
+
+const AuthPic = styled.div`
+  width: 600px;
+  height: 350px;
+  border-radius: 5px;
+  border: 1.5px dashed #b4b3b3;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "Pretendard";
+  color: #b4b3b3;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 700;
+  margin: 40px;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-grow: 1;
+  margin-bottom: 30px;
+  margin-right: 40px;
 `;
