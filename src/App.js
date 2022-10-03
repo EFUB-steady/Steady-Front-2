8ca3db74e86257d2e283d1fe6e4e0bf17853a3a5
SJ-Lee33 <77518932+SJ-Lee33@users.noreply.github.com>
2022-10-03 20:59:10 +0900

    ✍️ LoginPage 생성

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import MemberMainPage from "./pages/MainPage/MemberMainPage";
 import routes from "./routes/routes";
+import MemberMainPage from "./pages/MainPage/MemberMainPage";
+import LoginPage from "./pages/LoginPage";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={routes.MAIN} element={<MemberMainPage />} />
+        <Route path={routes.LOGIN} element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/pages/LoginPage/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Wrapper } from "./styles";
+// import DateAndAuth from "../../components/MemberMain/DateAndAuth";
+// import Header from "../../components/MemberMain/Header";
+// import Info from "../../components/MemberMain/Info";
+
+export default function LoginPage() {
+  return (
+    <Wrapper>
+      <div>좋냐</div>
+    </Wrapper>
+  );
+}
