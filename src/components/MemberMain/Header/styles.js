e87ac4d8899e0a48b348fb5d5f5fc1ab2421fe5e
journey_park <80277274+jihyeun-park@users.noreply.github.com>
2022-11-08 18:24:48 +0900

    Revert "Main page"

--- src/components/MemberMain/Header/styles.js
+++
@@ -1,1 +0,0 @@
-const LogoImg = styled.img``;
--- src/components/buttons/Joinbtn.jsx
+++ src/components/buttons/Joinbtn.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
-export default function JoinBtn() {
-  return <div>회원가입 버튼</div>;
+export default function Loginbtn() {
+    return (
+        <div>회원가입 버튼</div>
+    );
 }
+
