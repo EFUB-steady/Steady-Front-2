acd9b9eaf708b47438127bc3b9e2710287abbf0f
Sojin Lee <sojinlee0z0@gmail.com>
2022-11-29 20:23:05 +0900

    ✍️ 카카오로그인 버튼에 url 연결

--- src/components/buttons/KakaoLoginBtn/index.jsx
+++ src/components/buttons/KakaoLoginBtn/index.jsx
@@ -1,14 +1,26 @@
 import styled from "styled-components";
+import {
+  KAKAO_REDIRECT_URI,
+  KAKAO_REST_API_KEY,
+} from "../../../api/constants/config";
 import kakaoLoginBtn from "../../../assets/kakao_login_large_wide.png";
 
 export default function KakaoLoginBtn() {
+  const url = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_REST_API_KEY}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+
   return (
     <>
-      <KakaoLoginButton src={kakaoLoginBtn} />
+      <KakaoLoginButton
+        src={kakaoLoginBtn}
+        onClick={() => {
+          window.open(url, "_blank");
+        }}
+      />
     </>
   );
 }
 
 const KakaoLoginButton = styled.img`
   width: 300px;
+  cursor: pointer;
 `;
