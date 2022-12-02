f9116c2a7f9b4d232be0d568981253fbd11cdde2
Sojin Lee <sojinlee0z0@gmail.com>
2022-12-02 16:24:17 +0900

    ✍️ 스터디 만들기 - 인증 요일 선택

--- src/components/MakeStudy/StudyAuth/AuthDay.jsx
+++ src/components/MakeStudy/StudyAuth/AuthDay.jsx
@@ -1,13 +1,96 @@
 import React from "react";
 import styled from "styled-components";
+import { useMakeStudy } from "../../../api/recoil/MakeStudy/useMakeStudy";
+import { ewhaGreen } from "../../../styles/colors";
 
 export default function AuthDay() {
-  const DayList = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
-  const DayMap = DayList.map((DayList) => <DaySelect>{DayList}</DaySelect>);
+  const {
+    mon,
+    tue,
+    wed,
+    thu,
+    fri,
+    sat,
+    sun,
+    setMon,
+    setTue,
+    setWed,
+    setThu,
+    setFri,
+    setSat,
+    setSun,
+  } = useMakeStudy();
+
+  const DAYS = [
+    {
+      day: "Sun",
+      isClicked: sun,
+      onClick: () => {
+        setSun(!sun);
+      },
+    },
+    {
+      day: "Mon",
+      isClicked: mon,
+      onClick: () => {
+        console.log("mon");
+        setMon(!mon);
+      },
+    },
+    {
+      day: "Tue",
+      isClicked: tue,
+      onClick: () => {
+        console.log("tue");
+        setTue(!tue);
+      },
+    },
+    {
+      day: "Wed",
+      isClicked: wed,
+      onClick: () => {
+        console.log("wed");
+        setWed(!wed);
+      },
+    },
+    {
+      day: "Thu",
+      isClicked: thu,
+      onClick: () => {
+        console.log("thu");
+        setThu(!thu);
+      },
+    },
+    {
+      day: "Fri",
+      isClicked: fri,
+      onClick: () => {
+        console.log("fri");
+        setFri(!fri);
+      },
+    },
+    {
+      day: "Sat",
+      isClicked: sat,
+      onClick: () => {
+        console.log("sat");
+        setSat(!sat);
+      },
+    },
+  ];
+
   return (
     <Wrapper>
       <Info>인증 요일</Info>
-      {DayMap}
+      {DAYS.map((day) => (
+        <DaySelect
+          key={day.day}
+          isClicked={day.isClicked}
+          onClick={day.onClick}
+        >
+          {day.day}
+        </DaySelect>
+      ))}
     </Wrapper>
   );
 }
@@ -19,9 +102,16 @@ export const Wrapper = styled.div`
   padding-bottom: 20px;
 `;
 
-export const Info = styled.span`
+export const Info = styled.div`
+  margin-right: 50px;
 `;
 
 export const DaySelect = styled.div`
-  padding-left: 74px;
+  width: 50px;
+  text-align: center;
+  margin: 0 10px;
+  padding: 10px 0;
+  border-radius: 5px;
+  background-color: ${(props) => props.isClicked && ewhaGreen};
+  color: ${(props) => props.isClicked && "white"};
 `;
